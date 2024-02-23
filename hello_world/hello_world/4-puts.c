5f99fcdb3c2bb43d183374403abedc45497cd62e
Marc93z <marc-39@outlook.fr>
2024-02-23 16:31:36 +0000

    five project

--- hello_world/hello_world/4-puts.c
+++ hello_world/hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - afficher programme 
+ * Return: affiche le programme
+ */ 
 
-int main()
+int main(void) 
 {
- 
-    puts("Programming is like building a multilingual puzzle"); 
-    return 0;
+    
+    puts("\"Programming is like building a multilingual puzzle"); 
+
+    return (0);
 }
