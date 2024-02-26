55f5639b110d873192a91ea950dcad9f3661730d
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-02-26 15:25:00 +0100

    six project

---
+++ hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include(stdio.h) 
+
+int main(void) 
+{
+
+printf("char  :%d byte\"sizeof(char));
+printf("int  :%d byte \"sizeof(int));
+printf("long int  :%d byte\"sizeof(long int));
+printf("loong long int  :%d byte\"sizeof(long long int));
+printf("float  :%d byte\"sizeof(float));
+return(0);
+
+}
