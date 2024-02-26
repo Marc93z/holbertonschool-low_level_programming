f02454f1558b2d9be88a01353ffc6b921cdb544b
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-02-26 15:50:55 +0100

    six project

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@ int main(void)
 
 {
 
-
+printf("Size of a char  :%d byte \n",sizeof(char));
 printf("Size of a int  :%d byte \n",sizeof(int));
 printf("Size of a long int  :%d byte\n",sizeof(long int));
 printf("Size of a loong long int  :%d byte\n",sizeof(long long int));
