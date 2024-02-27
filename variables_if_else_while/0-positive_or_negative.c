68619d9d3bec19a892dc0e899be1960e19b1f237
Marc93z <marc-39@oulook.fr>
2024-02-27 16:10:07 +0000

    zero project

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 
         if(n < 0)
         {
-                printf("%d is positive\n",n);
+                printf("%d is negatif\n",n);
         }
         else if(n == 0)
         {
-                printf("%d is zero\n",n);
+                printf("%d is positif\n",n);
         }
         else
         {
-                printf("%d is negative\n",n);
+                printf("%d is zero\n",n);
         }
 
 	return (0);
