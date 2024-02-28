c6db4bd8fe6b76462d894670e34b64951a22ca4f
Marc93z <marc-39@outlook.fr>
2024-02-28 14:20:12 +0000

    2-print_alphabett

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+/**
+ * main - print alphabet
+ *
+ * Return: always 0 succes
+ */
+int main(void)
+{
+	char i = 'a';
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
