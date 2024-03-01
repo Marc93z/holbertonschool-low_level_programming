eeb83fa6c8de1be5642661947ed0365f79342550
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-01 13:20:56 +0100

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase alphabet
+ *
+ * Return: always 0 success
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
