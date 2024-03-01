12de3f87f8a8d9d2b6372a4784a6441ce33e5050
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-01 16:46:58 +0100

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * Return: value of last digit
+ */
+
+int print_last_digit(int c)
+{
+	int n = c % 10;
+
+	if (n < 0)
+	{
+		n *= -1;
+	}
+	_putchar(n + '0');
+
+	return (c % 10);
+}
