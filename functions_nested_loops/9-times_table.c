19af7ab077e4b17e98534caf264cbec839bbc86f
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-04 12:14:50 +0100

    9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,24 +1,34 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - Prints the 9 times table, starting with
+ *
+ * Return: times
+ */
+void times_table(void)
+{
+	int num, mult, prod;
 
-void times_table(int size) {
-    for (int i = 1; i <= size; i++) {
-        for (int j = 1; j <= size; j++) {
-            int result = i * j;
-            // Affichage du résultat avec putchar
-            if (result < 10) {
-                putchar(' '); // Ajoute un espace pour l'alignement
-                putchar(result + '0'); // Affiche le chiffre directement
-            } else {
-                putchar((result / 10) + '0'); // Affiche la dizaine
-                putchar((result % 10) + '0'); // Affiche l'unité
-            }
-            putchar(' '); // Ajoute un espace pour l'alignement
-        }
-        putchar('\n'); // Nouvelle ligne après chaque ligne de la table
-    }
-}
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
 
-int main() {
-    times_table(10); // Affiche une table de multiplication jusqu'à 10x10
-    return 0;
+			prod = num * mult;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
