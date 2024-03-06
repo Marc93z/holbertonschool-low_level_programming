28df9bf89afee55ea03d56a50e48972cf92eee02
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-06 16:59:48 +0100

    10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,17 +6,22 @@
  * Return: triangle
  */
 void print_triangle(int size)
+ 
 {
 	int i, a;
-	
-	if(size <= 0)
 
+	if(size <= 0)
+	{
+		_putchar('\n');
+	}
+	
 	for (i = 0 ; i < size ; i++)
 	{
 		for (a = i ; a < size ; a++)
 			{
 				_putchar(' ');
 			}
+		for (a = i ; a <= i; a++)
 			{
 				_putchar('#');
 			}
