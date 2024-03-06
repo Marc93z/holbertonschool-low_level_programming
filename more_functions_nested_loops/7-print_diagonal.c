20c902370ef4eaaef322dac2925f252561b17278
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-06 14:08:37 +0100

    7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,14 @@ void print_diagonal(int n)
 {
 	int j, k;
 
-	if (n <= 0)
+if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}	
 	for (j = 0 ; j < n ; j++)
 	{
-		for (k = 0 ; k < j ; k++)
+		for (k = 0 ; k < n ; k++)
 		{
 			if (j == k)	 
 			{
