f7cdc011b61ab2668789b949c5bfdd26cc5aaf64
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-07 16:03:03 +0100

    2-strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
 #include "main.h"
-
-int _strlen(char *s) {
-    int length = 0;
-
-    // Iterate over the string until the null terminator is encountered
-    while (*s != '\0') {
-        length++;
-        s++; // Move to the next character in the string
-    }
-
-    return length;
-} 
-
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - string length
+ * @s: parameter
+ *
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int len;
+	
+	len = strlen(s);
+	printf("%d\n", len);
+	return (0);
+}
