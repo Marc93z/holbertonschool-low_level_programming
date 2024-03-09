bb5e6be22365e4425d10ea4d49c1b3739976c9d8
Marc93z <159263992+Marc93z@users.noreply.github.com>
2024-03-09 11:55:02 +0100

    2-strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -12,7 +12,7 @@
 int _strlen(char *s) {
     int _strlen = 0;
 	int i;
-    // Iterate over the string until the null terminator is encountered
+
     for (i = 0; s[i] != '\0'; i++) {
 
         _strlen++;
