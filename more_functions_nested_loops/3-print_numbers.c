a73b5d14372051b153d91ba0cfbb5034feb527ac
Marc93z <marc-39@outlook.fr>
2024-06-25 13:33:11 +0200

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print of 0 to 9
+ *
+ * Return: 0 is succes
+ */
+void print_numbers(void)
+{
+        char i;
+
+        for (i = '0' ; i <= '9' ; i++)
+        {
+                _putchar (i);
+      }
+        _putchar('\n');
+}
