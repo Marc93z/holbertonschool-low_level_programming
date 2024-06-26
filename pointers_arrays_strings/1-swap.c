307f0ad4826bf6866163aa8adb585909c77da41f
Marc93z <marc-39@outlook.fr>
2024-06-26 15:03:24 +0200

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swap two values
+ * @a: first value at swap
+ * @b: second value at swap
+ *
+ * Return: swapping
+ */
+void swap_int(int *a, int *b)
+{
+        int i = *a;
+        int j = *b;
+       *a = j;
+        *b = i;
+}
